45b2613d3f78ef787e86e2f7cd6e6acb868f76ed
AMiro <aj.miro.b@gmail.com>
2023-06-12 18:53:37 -0500

    Fix profile pic display issue

--- src/components/About.js
+++ src/components/About.js
@@ -19,16 +19,17 @@ export default function About() {
                 <a
                   href="#projects"
                   className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                  See My Past Work
+                  See My Stuff
                 </a>
               </div>
             </div>
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-              {/* fix img display issue */}
               <img
                 className="object-cover object-center rounded"
                 alt="It's-a me!"
-                src="./andres.jpg"
+                src="./andres.svg"
+                style={{ width: '200px', height: '300px' }}
+
               />
             </div>
           </div>
